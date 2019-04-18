84dc543c74acf5abf9aaa9808def09f95de47357
Vitaliy Ilin <ilin_vitaliy@inbox.ru>
2019-04-18 18:41:36 +0700

    Realize function proverki ypor

---
+++ check.c
@@ -0,0 +1,16 @@
+#include "pyatnashki.h"
+
+void check(int (*a)[4],int N,bool &flag )
+{
+    int i,m,j;
+    for( i = 0, flag = 1, m = 1; i < N; i++){
+        for(int j = 0; j < 4; j++){
+            if(m < 16 && a[i][j] != m){
+
+                flag = 0;
+                break;
+            }
+            m++;
+        }
+    }
+}
