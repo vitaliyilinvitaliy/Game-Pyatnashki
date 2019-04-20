1c8492aea7f0840132674288c258a1bad91a078d
MaksimDov <maksimdovydenko@mail.ru>
2019-04-20 15:14:53 +0700

    Add move in main.c

--- src/main.c
+++ src/main.c
@@ -14,5 +14,58 @@ int main() {
 	int pyat[4][4];
 	zapolnenie(a, pyat, 5);
 	position(pyat, 4, ik, jk);
+
+	while (1) {
+		print(pyat, 4);
+		check(pyat, 4, flag);
+
+		switch (getch()) {
+			case 'a':
+				if (jk != 0) {
+					temp = pyat[ik][jk];
+					pyat[ik][jk] = pyat[ik][jk - 1];
+					pyat[ik][jk - 1] = temp;
+					jk--;
+					break;
+				} else {
+					break;
+				}
+			case 'w':
+				if (ik != 0) {
+					temp = pyat[ik][jk];
+					pyat[ik][jk] = pyat[ik - 1][jk];
+					pyat[ik - 1][jk] = temp;
+					ik--;
+					break;
+				} else {
+					break;
+				}
+			case 'd':
+				if (jk != 3) {
+					temp = pyat[ik][jk];
+					pyat[ik][jk] = pyat[ik][jk + 1];
+					pyat[ik][jk + 1] = temp;
+					jk++;
+					break;
+				} else {
+					break;
+				}
+			case 's':
+				if (ik != 3) {
+					temp = pyat[ik][jk];
+					pyat[ik][jk] = pyat[ik + 1][jk];
+					pyat[ik + 1][jk] = temp;
+					ik++;
+					break;
+				} else {
+					break;
+				}
+		}
+		system("clear");
+		if (flag) {
+			cout << "You won!!!!!" << endl;
+			break;
+		};
+	}
 	return 0;
 }
