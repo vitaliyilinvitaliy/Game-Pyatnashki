85cd2adadc7298693fe5f90f06299b9dc8dd747a
VitaliyIlin <ilin_vitaliy@inbox.ru>
2019-04-25 22:27:02 +0700

    Ubral {..} in menu.c

--- src/menu.c
+++ src/menu.c
@@ -12,40 +12,40 @@ void menu()
     while (1) {
         cout << "1.Start\n2.Rules\n3.Records\n4.Exit\n";
         switch (getch()) {
-        case '1': {
+        case '1':
             flag = 1;
             system("clear");
             break;
-        }
-        case '2': {
+
+        case '2':
             system("clear");
             rules.open("../Rul/Rules.txt");
             if (!rules.is_open()) {
                 cout << "error" << endl;
                 ;
                 exit(1);
-            };
-            while (!rules.eof()) {
-                rules >> buff;
-                cout << buff << " ";
             }
+            while (!rules.eof())
+                rules >> buff;
+            cout << buff << " ";
+
             rules.close();
             cout << "\n\t...Press to key...\n";
             if (getch())
                 system("clear");
 
             break;
-        }
-        case '3': {
+
+        case '3':
             system("clear");
             view(tf);
             system("clear");
             break;
-        };
-        case '4': {
+
+        case '4':
             exit(1);
         }
-        }
+
         if (flag)
             break;
         system("clear");
