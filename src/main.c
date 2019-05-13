0a7783a8176c95969e1b6c6e675d24190892351b
VitaliyIlin <ilin_vitaliy@inbox.ru>
2019-05-13 23:34:02 +0700

    Add fon for menu and created icon for window

--- src/main.c
+++ src/main.c
@@ -5,7 +5,12 @@ int main()
 
 
     RenderWindow window(VideoMode(660, 371), "PYATNASHKI-GAME");
-
+    Image icon;
+    if (!icon.loadFromFile("../images/15.jpg"))
+    {
+    return 1;
+    }
+    window.setIcon(62, 55, icon.getPixelsPtr());	
     return 0;
 }
 
