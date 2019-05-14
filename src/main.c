519bdc3ccafd68c7ce8cad8390033d31df5e1e9d
VitaliyIlin <ilin_vitaliy@inbox.ru>
2019-05-15 01:21:16 +0700

    Made the same style code (clang-format-6.0)

--- src/main.c
+++ src/main.c
@@ -1,18 +1,14 @@
 #include "pyatnashki.h"
 
-extern void menu(RenderWindow&);
+extern void menu(RenderWindow &);
 
-int main()
-{
-    RenderWindow window(VideoMode(660, 371), "PYATNASHKI-GAME");
-    
-    Image icon;
-    if (!icon.loadFromFile("../images/15.jpg"))
-    {
+int main() {
+  RenderWindow window(VideoMode(660, 371), "PYATNASHKI-GAME");
+
+  Image icon;
+  if (!icon.loadFromFile("../images/15.jpg")) {
     return 1;
-    }
-    window.setIcon(62, 55, icon.getPixelsPtr());	
-    menu(window);
-
+  }
+  window.setIcon(62, 55, icon.getPixelsPtr());
+  menu(window);
 }
-
--- src/menu.c
+++ src/menu.c
@@ -1,132 +1,125 @@
 #include "pyatnashki.h"
 
-extern void records(RenderWindow&, bool&);
-extern void play_game(RenderWindow &,bool&);
+extern void records(RenderWindow &, bool &);
+extern void play_game(RenderWindow &, bool &);
 
+void menu(RenderWindow &window) {
+  bool flag_close = false;
+  Texture fon;
+  fon.loadFromFile("../images/fon.jpg");
+  Sprite fonsprite(fon);
 
-void menu(RenderWindow& window) {
-	bool flag_close = false;
-	Texture fon;
-	fon.loadFromFile("../images/fon.jpg");
-	Sprite fonsprite(fon);
+  fonsprite.setPosition(0, 0);
 
-	fonsprite.setPosition(0, 0);
+  Texture rul;
+  rul.loadFromFile("../images/rules.jpg");
+  Sprite rulsprite(rul);
 
-	Texture rul;
-	rul.loadFromFile("../images/rules.jpg");
-	Sprite rulsprite(rul);
+  rulsprite.setPosition(0, 0);
 
-	rulsprite.setPosition(0, 0);
+  Font font;
+  if (!font.loadFromFile("../font/ar2.ttf")) {
+    // ошибка...
+  }
+  Text text1;
+  Text text2;
+  Text text3;
+  Text text4;
+  // start
+  text1.setFont(font);
+  text1.setString("START");
+  text1.setCharacterSize(60);
+  text1.setColor(Color::White);
 
-	Font font;
-	if (!font.loadFromFile("../font/ar2.ttf")) {
-		// ошибка...
-	}
-	Text text1;
-	Text text2;
-	Text text3;
-	Text text4;
-	// start
-	text1.setFont(font);
-	text1.setString("START");
-	text1.setCharacterSize(60);
-	text1.setColor(Color::White);
+  // rules
+  text2.setFont(font);
+  text2.setString("RULES");
+  text2.setCharacterSize(60);
+  text2.setColor(Color::White);
 
-	// rules
-	text2.setFont(font);
-	text2.setString("RULES");
-	text2.setCharacterSize(60);
-	text2.setColor(Color::White);
+  // records
+  text3.setFont(font);
+  text3.setString("RECORDS");
+  text3.setCharacterSize(60);
+  text3.setColor(Color::White);
 
-	// records
-	text3.setFont(font);
-	text3.setString("RECORDS");
-	text3.setCharacterSize(60);
-	text3.setColor(Color::White);
+  // exit
+  text4.setFont(font);
+  text4.setString("EXIT");
+  text4.setCharacterSize(60);
+  text4.setColor(Color::White);
 
-	// exit
-	text4.setFont(font);
-	text4.setString("EXIT");
-	text4.setCharacterSize(60);
-	text4.setColor(Color::White);
+  bool isMenu = 1;
+  int menuNum = 0;
+  text1.setPosition(150, 5);
+  text2.setPosition(150, 55);
+  text3.setPosition(150, 105);
+  text4.setPosition(150, 160);
 
-	bool isMenu = 1;
-	int menuNum = 0;
-	text1.setPosition(150, 5);
-	text2.setPosition(150, 55);
-	text3.setPosition(150, 105);
-	text4.setPosition(150, 160);
+  //////////////////////////////МЕНЮ///////////////////
+  while (isMenu) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+        isMenu = false;
+      }
+    }
+    text1.setColor(Color::White);
+    text2.setColor(Color::White);
+    text3.setColor(Color::White);
+    text4.setColor(Color::White);
+    menuNum = 0;
+    window.clear();
 
-	//////////////////////////////МЕНЮ///////////////////
-	while (isMenu) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-				isMenu = false;
-			}
-		}
-		text1.setColor(Color::White);
-		text2.setColor(Color::White);
-		text3.setColor(Color::White);
-		text4.setColor(Color::White);
-		menuNum = 0;
-		window.clear();
+    if (IntRect(150, 30, 170, 45).contains(Mouse::getPosition(window))) {
+      text1.setColor(Color(0, 202, 0));
+      menuNum = 1;
+    }
+    if (IntRect(150, 82, 170, 45).contains(Mouse::getPosition(window))) {
+      text2.setColor(Color(0, 202, 0));
+      menuNum = 2;
+    }
+    if (IntRect(150, 135, 170, 45).contains(Mouse::getPosition(window))) {
+      text3.setColor(Color(0, 202, 0));
+      menuNum = 3;
+    }
+    if (IntRect(150, 180, 170, 45).contains(Mouse::getPosition(window))) {
+      text4.setColor(Color(0, 202, 0));
+      menuNum = 4;
+    }
 
-		if (IntRect(150, 30, 170, 45)
-			.contains(Mouse::getPosition(window))) {
-			text1.setColor(Color(0, 202, 0));
-			menuNum = 1;
-		}
-		if (IntRect(150, 82, 170, 45)
-			.contains(Mouse::getPosition(window))) {
-			text2.setColor(Color(0, 202, 0));
-			menuNum = 2;
-		}
-		if (IntRect(150, 135, 170, 45)
-			.contains(Mouse::getPosition(window))) {
-			text3.setColor(Color(0, 202, 0));
-			menuNum = 3;
-		}
-		if (IntRect(150, 180, 170, 45)
-			.contains(Mouse::getPosition(window))) {
-			text4.setColor(Color(0, 202, 0));
-			menuNum = 4;
-		}
+    if (Mouse::isButtonPressed(Mouse::Left)) {
+      if (menuNum == 1) {
+        play_game(window, isMenu);
+      }
+      if (menuNum == 2) {
+        window.draw(rulsprite);
+        window.display();
+        while (!Keyboard::isKeyPressed(Keyboard::Escape)) {
+          while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+              window.close();
+              return;
+            }
+          }
+        }
+      }
+      if (menuNum == 3) {
+        records(window, isMenu);
+      }
+      if (menuNum == 4) {
+        window.close();
+        isMenu = false;
+      }
+    }
 
-		if (Mouse::isButtonPressed(Mouse::Left)) {
-			if (menuNum == 1) {
-				play_game(window,isMenu);
-			}
-			if (menuNum == 2) {
-				window.draw(rulsprite);
-				window.display();
-				while (
-				    !Keyboard::isKeyPressed(Keyboard::Escape)) {
-					while (window.pollEvent(event)) {
-						if (event.type ==
-						    sf::Event::Closed) {
-							window.close();
-							return;
-						}
-					}
-				}
-			}
-			if (menuNum == 3) {
-				records(window, isMenu);
-			}
-			if (menuNum == 4) {
-				window.close();
-				isMenu = false;
-			}
-		}
+    window.draw(fonsprite);
+    window.draw(text1);
+    window.draw(text2);
+    window.draw(text3);
+    window.draw(text4);
 
-		window.draw(fonsprite);
-		window.draw(text1);
-		window.draw(text2);
-		window.draw(text3);
-		window.draw(text4);
-
-		window.display();
-	}
+    window.display();
+  }
 }
