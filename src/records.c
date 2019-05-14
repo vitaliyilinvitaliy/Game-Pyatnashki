39c3bc3e5ba4630289e278aee0b6f2241ffc8bd3
MaksimDov <maksimdovydenko@mail.ru>
2019-05-14 15:40:54 +0700

    Text del(records)

--- src/records.c
+++ src/records.c
@@ -19,4 +19,11 @@ void records(RenderWindow& app, bool& flag_close) {
 	head.setCharacterSize(45);
 	head.setColor(Color::Green);
 	head.setPosition(50, 5);
+
+	Text del;
+	del.setFont(font);
+	del.setString("To clear the list of records, press the key 'Delete'");
+	del.setCharacterSize(30);
+	del.setColor(Color::Red);
+	del.setPosition(50, 310);
 }
