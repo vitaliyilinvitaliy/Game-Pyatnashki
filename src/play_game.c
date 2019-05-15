925d7bdd1b9b8e094da69faebe70752f2f51b832
VitaliyIlin <ilin_vitaliy@inbox.ru>
2019-05-15 20:59:56 +0700

    Added images for win, sound(yes,no) + sprite in play_game.c

--- src/play_game.c
+++ src/play_game.c
@@ -5,13 +5,27 @@ void play_game(RenderWindow &window, bool &flag_close) {
   vector<int> a(16);
   int pyat[4][4];
 
-  Texture fon_game;
-  fon_game.loadFromFile("../images/fon_game.jpg");
-  Sprite fon_gamesprite(fon_game);
+   Texture fon_game;
+   fon_game.loadFromFile("../images/fon_game.jpg");
+   Sprite fon_gamesprite(fon_game);
 
-  Texture p;
-  p.loadFromFile("../images/p2.jpg");
-  Sprite psprite(p);
+   Texture p;
+   p.loadFromFile("../images/p2.jpg");
+   Sprite psprite(p);
+
+   Texture win;
+   win.loadFromFile("../images/win2.jpg");
+   Sprite winsprite(win);
+
+   Texture sound_yes;
+   sound_yes.loadFromFile("../images/sound_yes.png");
+   Sprite sound_yes_sprite(sound_yes);
+   sound_yes_sprite.setPosition(600,10);
+
+   Texture sound_no;
+   sound_no.loadFromFile("../images/sound_no.png");
+   Sprite sound_no_sprite(sound_no);
+   sound_no_sprite.setPosition(600,10);
 
   while (!Keyboard::isKeyPressed(Keyboard::Escape)) {
 
