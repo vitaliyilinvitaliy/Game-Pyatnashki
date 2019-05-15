790da62a85495517481cfeb44b4f293e6c2554e4
VitaliyIlin <ilin_vitaliy@inbox.ru>
2019-05-15 19:29:01 +0700

    Added SFML/ in pyatnashki.h

--- src/pyatnashki.h
+++ src/pyatnashki.h
@@ -1,10 +1,10 @@
 #ifndef PYATNASHKI_H
 #define PYATNASHKI_H
 
-#include <Audio.hpp>
-#include <Graphics.hpp>
-#include <System.hpp>
-#include <Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 
 #include <algorithm>
 #include <ctime>
